3629d584d5c522163ea18fefd62e3cde07cf3b06
miguel.silva <miguel.silva@vilt-group.com>
2020-12-01 21:21:04 +0000

    fix

--- go/2020/puzzle/day1solver.go
+++ go/2020/puzzle/day1solver.go
@@ -17,51 +17,57 @@ func NewDay1Solver() solver.Solver {
 	return &Day1{Target: 2020}
 }
 
-// ReadInput
+// ProcessInput of day 1
 func (d *Day1) ProcessInput(lines []string) error {
 	for _, line := range lines {
 		i, err := strconv.Atoi(line)
+
 		if err != nil {
 			return fmt.Errorf("couldn't convert %s to int : %v", line, err)
 		}
 
 		d.Entries = append(d.Entries, i)
 	}
-
 	return nil
 }
 
+// Part1 of day 1
 func (d *Day1) Part1() (string, error) {
-	var arr []int
-	entry1, entry2 := -1, -1
-	for _, fst := range d.Entries {
-		for _, snd := range arr {
-			if fst+snd == d.Target {
-				entry1 = fst
-				entry2 = snd
-				break
+	var entry1, entry2 int
+	leng := len(d.Entries)
+
+	for i := 0; i < leng-1; i++ {
+		for j := i + 1; j < leng; j++ {
+			entry1 = d.Entries[i]
+			entry2 = d.Entries[j]
+
+			if entry1+entry2 == d.Target {
+				return strconv.Itoa(entry1 * entry2), nil
 			}
 		}
-		if entry1 != -1 {
-			break
-		}
-		arr = append(arr, fst)
 	}
-	return fmt.Sprintf("%d", entry1*entry2), nil
+
+	return "None", nil
 }
 
+// Part2 of day 1
 func (d *Day1) Part2() (string, error) {
-	entry1, entry2, entry3 := -1, -1, -1
+	var entry1, entry2, entry3 int
 	leng := len(d.Entries)
-	for i := 0; i < leng-2 && entry1+entry2+entry3 != d.Target; i++ {
-		for j := i + 1; j < leng-1 && entry1+entry2+entry3 != d.Target; j++ {
-			for k := i + 2; k < leng && entry1+entry2+entry3 != d.Target; k++ {
+
+	for i := 0; i < leng-2; i++ {
+		for j := i + 1; j < leng-1; j++ {
+			for k := i + 2; k < leng; k++ {
 				entry1 = d.Entries[i]
 				entry2 = d.Entries[j]
 				entry3 = d.Entries[k]
+
+				if entry1+entry2+entry3 == d.Target {
+					return strconv.Itoa(entry1 * entry2 * entry3), nil
+				}
 			}
 		}
 	}
 
-	return fmt.Sprintf("%d", entry1*entry2*entry3), nil
+	return "None", nil
 }
