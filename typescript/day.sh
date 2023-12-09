454430ee184062c0f59df84912a70b7877a5bcc5
Miguel Silva <silva.gp.miguel@gmail.com>
2023-12-09 16:53:28 +0000

    feat: add npm script to create a new day of a specific year and day

---
+++ typescript/day.sh
@@ -0,0 +1,19 @@
+YEAR=$1
+DAY=$2
+
+if [ -z "$YEAR" ] || [ -z "$DAY" ]; then
+    echo "Error: npm run day <YEAR> <DAY>"
+else
+    if ! cat ./libs/$YEAR/index.ts | grep -q "day$DAY"; then 
+        echo "export * from './day$DAY'" >> ./libs/$YEAR/index.ts
+    fi
+    touch ../input/$YEAR/$DAY.1 ../input/$YEAR/$DAY.2
+    echo "
+import { StringArrayInputParts, Day } from '@advent/utils'
+
+export const day6_2023 = ({ part1, part2 }: StringArrayInputParts): Day => ({
+  part1: (): number => 0,
+  part2: (): number => 0,
+})
+" > ./libs/$YEAR/day$DAY.ts
+fi
